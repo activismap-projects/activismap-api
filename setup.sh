eb00829250dfe3a85b5f50fd2473198c0e2a3b1f
Lluis Santos <cto@entropy-factory.com>
2016-02-21 00:44:52 +0100

    installed fos_user bundle

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+# This script changes the namespace for the bundle
+
+USAGE="\e[34mUsage: \e[32m$0 <NameBundle>\e[0m"
+ARGNUM=1
+
+if [[ $# != $ARGNUM ]];then
+  echo -e "\e[34mERROR: \e[31mThis program takes exactly $ARGNUM arguments\e[0m" >&2
+  echo -e $USAGE >&2
+  exit 1
+fi
+
+OLDBUNDLENAME=`find src/ -type d -name *Bundle -exec basename ̣{} \;`
+NEWBUNDLENAME=$1
+
+
+echo "Working..."
+mv src/$OLDBUNDLENAME src/$NEWBUNDLENAME
+mv src/$NEWBUNDLENAME/$OLDBUNDLENAME.php src/$NEWBUNDLENAME/$NEWBUNDLENAME.php
+find src -type f -exec sed "s/$OLDBUNDLENAME/$NEWBUNDLENAME/g" -i {} \;
+find app -type f -exec sed "s/$OLDBUNDLENAME/$NEWBUNDLENAME/g" -i {} \;
+
+
+#php bin/console doctrine:schema:update --force
+#create database test;
+#grant all privileges on test.* to test@localhost identified by 'test';
+
+
+
+
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,37 @@
+<?php
+// src/AppBundle/Entity/User.php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+  /**
+   * @ORM\Id
+   * @ORM\Column(type="integer")
+   * @ORM\GeneratedValue(strategy="AUTO")
+   */
+  protected $id;
+
+
+  /**
+   * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Group")
+   * @ORM\JoinTable(name="fos_user_user_group",
+   *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
+   *      inverseJoinColumns={@ORM\JoinColumn(name="group_id", referencedColumnName="id")}
+   * )
+   */
+  protected $groups;
+
+  public function __construct()
+  {
+    parent::__construct();
+    // your own logic
+  }
+}

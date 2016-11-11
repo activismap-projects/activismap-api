ca42f369f04ddeec9518d0b8cad80cb79ea81d3f
Andersson <ander7agar@gmail.com>
2016-11-11 16:42:42 +0100

    corregido limites de busqueda

--- src/ActivisMap/Controller/PublicController.php
+++ src/ActivisMap/Controller/PublicController.php
@@ -105,7 +105,7 @@ class PublicController extends Neo4jController {
         $endDate = 0;
         $startDate = 0;
         $limit = 20;
-        $offset = 0;
+        $offset = 1;
 
         if (array_key_exists('end_date', $params)) {
             $endDate = intval($params['end_date']);
